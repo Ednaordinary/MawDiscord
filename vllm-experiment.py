3706a846cc94a1a70a81932fbcb67eb016c04153
BuildTools <imnotednamode@gmail.com>
2024-07-23 03:15:22 -0600

    sleepy timeee

--- vllm-experiment.py
+++ vllm-experiment.py
@@ -2,7 +2,8 @@ from vllm import LLM, SamplingParams
 import time
 
 sampling_params = SamplingParams(temperature=0.6, top_p=0.9)
-model = LLM(model="failspy/Meta-Llama-3-8B-Instruct-abliterated-v3", speculative_model="[ngram]", num_speculative_tokens=15, ngram_prompt_lookup_max=4, quantization="fp8", use_v2_block_manager=True) #, cpu_offload_gb=10)
+#failspy/Meta-Llama-3-8B-Instruct-abliterated-v3
+model = LLM(model="failspy/Meta-Llama-3-70B-Instruct-abliterated-v3.5", speculative_model="[ngram]", num_speculative_tokens=15, ngram_prompt_lookup_max=4, quantization="fp8", use_v2_block_manager=True, cpu_offload_gb=55)
 start_time = time.time()
 outputs = model.generate("Who is Edna Mode?", sampling_params)
 end_time = time.time()
