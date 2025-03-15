5e026a0da515f6547d0eb81e9f1d3cd27e74f1fb
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-03-15 01:35:46 -0600

    ensure types, try to handle stuff better

--- character/history.py
+++ character/history.py
@@ -5,8 +5,8 @@ role_trans_rev = {"u": "user", "s": "system", "c": "assistant"}
 
 class Message:
     def __init__(self, message_id, content, role):
-        self.message_id = message_id
-        self.content = content
+        self.message_id = int(message_id)
+        self.content = str(content)
         self.role = role
 
 class History:
@@ -19,15 +19,17 @@ class History:
         with open(self.path, "w") as f:
             f.write("")
         if self.sys != None:
-            self.append_message(Message(0, self.sys, "system"))
+            self.add_message(Message(0, self.sys, "system"))
             self.write_history()
     def write_history(self):
+        print(len(self.history))
         with open(self.path, "w") as file:
             for message in self.history:
                 content = (role_trans[message.role] if message.role in role_trans.keys() else "c") + "|"
                 content = content + str(message.message_id) + "|"
                 content = content + message.content.replace("\n", r"\\n").replace("\r", r"\\n") + "\n" 
                 file.write(content)
+        self.read_history()
     def read_history(self, limit=None):
         self.history = []
         if os.path.isfile(self.path):
@@ -40,10 +42,18 @@ class History:
         else:
             self.touch_history()
         if limit != None:
-            self.history.sort(key=lambda x: x.message_id)
+            #self.history.sort(key=lambda x: x.message_id)
             return [x for x in self.history if x.message_id <= limit]
         else:
             return self.history
+    def dedupe(self):
+        self.read_history()
+        messages = {}
+        for x in self.history:
+            messages[x.message_id] = x
+        self.history = messages.values()
+        self.write_history()
+        self.sort_messages()
     def append_message(self, message):
         self.read_history()
         if message.message_id not in [x.message_id for x in self.history]:
@@ -51,7 +61,7 @@ class History:
             self.write_history()
     def add_message(self, message):
         self.append_message(message)
-        self.sort_messages()
+        self.dedupe()
     def sort_messages(self):
         # Message IDs are in order, which we can use to our advantage
         self.read_history()
@@ -59,9 +69,7 @@ class History:
         self.write_history()
     def remove_message(self, message_id):
         self.read_history()
-        for idx, i in enumerate(self.history):
-            if i.message_id == message_id:
-                self.history.pop(idx)
+        self.history = [x for x in self.history if x.message_id != message_id]
         self.write_history()
         self.sort_messages()
     def edit_message(self, message):
@@ -71,8 +79,12 @@ class History:
         else:
             for idx, i in enumerate(self.history):
                 if i.message_id == message.message_id:
-                    self.history[idx].content = message.content
-        self.write_history()
+                    role = i.role
+                    self.remove_message(message.message_id)
+                    self.add_message(Message(message.message_id, message.content, role))
+                    break
+            self.dedupe()
+            self.write_history()
     def to_tokenizer(self, group=True, grouper="\n", limit=None):
         if group:
             history = []
@@ -91,6 +103,6 @@ class History:
             return history
         else:
             history = []
-            for i in self.read_history():
+            for i in self.read_history(limit=limit):
                 history.append({"role": i.role, "content": i.content})
             return history
