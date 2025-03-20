e60d1396de5d613f83439e150b4e7044b586a698
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-03-19 22:33:33 -0600

    Add status, dev check, dante call

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from exllamav2 import ExLlamaV2Cache_Q4
 from queue import Queue
 import nextcord as discord
 import threading
+import asyncio
 import time
 import os
 
@@ -13,20 +14,32 @@ from modeling.tokenize import Tokenizer
 from character.request import CharacterRequest
 from character.history import History, Message
 from character.defaults import MawPrompts
+from character.tools import DanteTool
 
-from util import init, get_path, get_all_chars, is_referring
+from util import init, get_path, get_all_chars, is_referring, dev_check, perm_check, get_hook
 
-client, token = init()
+client, token, dev_mode, dante_id = init()
+owner = None
+tokens = 0
+run_time = 0
+hooks = {}
 
 cutoff = 1024 * 15
 model_loop = Exl2Loop()
-model_handler = Exl2ModelHandlerLazy("dr1-32b-abliterated-exl2-4.0bpw-hb8", 1024 * 60, ExLlamaV2Cache_Q4, model_loop)
+model_handler = Exl2ModelHandlerLazy("dr1-32b-abliterated-exl2-4.0bpw-hb8", 1024 * 35, ExLlamaV2Cache_Q4, model_loop)
 tokenizer = Tokenizer("deepseek-ai/DeepSeek-R1-Distill-Qwen-32B")
 
 character_queue = Queue()
-handled_channels = {}
+handlers = 0
 
 def handler(to_handle):
+    global handlers, tokens, run_time
+    handlers += 1
+    if run_time != 0:
+        activity = discord.Activity(type=discord.ActivityType.watching, name="at " + str(int(tokens / run_time)) + " avg tps")
+        asyncio.run_coroutine_threadsafe(coro=client.change_presence(activity=activity, status=discord.Status.online), loop=client.loop)
+    else:
+        asyncio.run_coroutine_threadsafe(coro=client.change_presence(status=discord.Status.online), loop=client.loop)
     limiter = time.perf_counter()
     for i in model_handler.allocate(progress = True):
         if isinstance(i, tuple):
@@ -41,8 +54,13 @@ def handler(to_handle):
     if model == None:
         print("Model was not properly allocated")
         return
-    to_handle.handle(model, tokenizer, client.loop, character_queue)
-    model_handler.deallocate()
+    tokens += to_handle.handle(model, tokenizer, client.loop, character_queue)
+    run_time += model_handler.deallocate()
+    handlers -= 1
+    if handlers == 0:
+        if run_time != 0:
+            activity = discord.Activity(type=discord.ActivityType.watching, name="at " + str(int(tokens / run_time)) + " avg tps")
+        asyncio.run_coroutine_threadsafe(coro=client.change_presence(status=discord.Status.idle, activity=activity), loop=client.loop)
 
 def character_watcher():
     global character_queue
@@ -61,12 +79,17 @@ async def on_message(message):
         char_message = True
     elif "maw," in message.content.lower() or "<@" + str(client.user.id) + ">" in message.content or is_referring(message, client.user):
         maw_message = True
-    if maw_message:
-        bot_message = await message.channel.send("...")
-        history = History(get_path("maw", "history", char_id=message.channel.id, server_id=message.guild.id if message.guild else None), MawPrompts.default)
-        prompt = str(message.author.nick or message.author.global_name or message.author.name or "User").strip() + " said: " + message.clean_content
-        global character_queue
-        character_queue.put(CharacterRequest(message, bot_message, history, prompt, cutoff, [], False))
+    if maw_message and dev_check(dev_mode, owner, message.author):
+        if perm_check(message.channel, message.guild.me, "send"):
+            bot_message = await message.channel.send("...")
+            history = History(get_path("maw", "history", char_id=message.channel.id, server_id=message.guild.id if message.guild else None), MawPrompts.default)
+            prompt = str(message.author.nick or message.author.global_name or message.author.name or "User").strip() + " said: " + message.clean_content
+            global character_queue
+            dante_tool = DanteTool(get_hook, dante_id, check_perm, message.channel, hooks, client.loop, client.id)
+            character_queue.put(CharacterRequest(message, bot_message, history, prompt, cutoff, [dante_tool], False))
+    elif maw_message:
+        if perm_check(message.channel, message.guild.me, "send"):
+            await message.channel.send("### >>> Maw is in dev mode. Please come back later.")
 
 @client.event
 async def on_raw_message_edit(payload):
@@ -96,6 +119,9 @@ async def on_raw_message_delete(payload):
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
     await client.change_presence(status=discord.Status.idle)
+    global owner
+    owner = await client.application_info()
+    owner = owner.owner
 
 print("Starting up..")
 threading.Thread(target=character_watcher).start()
