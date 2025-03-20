8d4d63eff96353a788991cdc5a1e426bd12fcc44
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-03-19 22:33:52 -0600

    Add more utils

--- util.py
+++ util.py
@@ -6,6 +6,8 @@ import torch
 def init():
     load_dotenv()
     token = os.getenv('DISCORD_TOKEN')
+    dev_mode = True if os.getenv('DEV_MODE') == "1" else False
+    dante_id = int(os.getenv('DANTE_ID'))
     intents = discord.Intents.all()
     client = discord.AutoShardedClient(intents=intents)
     discord.opus._load_default()
@@ -13,7 +15,7 @@ def init():
     os.environ["TOKENIZERS_PARALLELISM"] = "1"
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
-    return client, token
+    return client, token, dev_mode, dante_id
 
 def get_path(path_type="maw", data_type = "history", char_id=None, server_id=None):
     if path_type == "maw":
@@ -54,3 +56,28 @@ def is_referring(message, user):
             return False
     except:
         return False
+
+def dev_check(dev_mode, owner, user):
+    if dev_mode:
+        if owner.id == user.id:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+def perm_check(channel, user, perm):
+    if perm == "send":
+        return channel.permissions_for(user).send_messages
+
+def get_hook(channel, hooks, loop, client_id):
+    try:
+        hook = hooks[channel.id]
+    except:
+        all_hooks = asyncio.run_coroutine_threadsafe(coro=channel.webhooks(), loop=client.loop).result()
+        for each_hook in all_hooks:
+            if each_hook.user.id == client_id:
+                asyncio.run_coroutine_threadsafe(coro=each_hook.delete(), loop=client.loop)
+        hook = asyncio.run_coroutine_threadsafe(coro=channel.create_webhook(name="Character hook"), loop=client.loop).result()
+        hooks[channel.id] = hook
+    return hook
