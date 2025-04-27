8a01437c0eacce5a3c24079d3f7e5de5ecac2bec
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-04-27 05:31:29 -0600

    update tools

--- character/tools.py
+++ character/tools.py
@@ -8,14 +8,14 @@ class Tool:
         return answer
     def will_run(self, answer):
         return False
-    def run(self, answer):
+    async def run(self, answer):
         return
 
 class DanteTool(Tool):
     doc = "You are able to generate or create images by asking another assistant known as Dante: simply enclose a description of an image in <- and ->, for example: <-A hot dog on a grill, the grill sits on a wooden table with condiments on it->. If asked to make an image, go ahead and generate it"
     def __init__(self, get_hook, dante_id, check_perm, channel, hooks, loop, client_id):
         super().__init__()
-        self.regex = re.compile(r'<-[\S\s]+>')
+        self.regex = re.compile(r'<-[\S\s]+?>')
         self.get_hook = get_hook
         self.dante_id = dante_id
         self.check_perm = check_perm
@@ -24,12 +24,12 @@ class DanteTool(Tool):
         self.loop = loop
         self.client_id = client_id
     def filter(self, answer):
-        return re.sub(r'<-[\S\s]+>', "", answer)
+        return re.sub(self.regex, "", answer)
     def will_run(self, answer):
         for i in re.findall(self.regex, answer):
             return True
         return False
-    def run(self, answer):
+    async def run(self, answer):
         try:
             members = self.channel.members
             hook = True
@@ -38,7 +38,9 @@ class DanteTool(Tool):
                     if self.check_perm(self.channel, i, "send"):
                         hook = False
             if hook:
-                hook = self.get_hook(self.channel, self.hooks, self.loop, self.client_id)
+                print("getting hook")
+                hook = await self.get_hook(self.channel, self.hooks, self.client_id)
+                print("got hook")
                 if hook == None:
                     return
             for image in re.findall(self.regex, answer):
@@ -46,6 +48,7 @@ class DanteTool(Tool):
                 if image[-1] == "-": image = image[:-1]
                 if image.strip() != "":
                     try:
+                        print("writing to queue")
                         with open("../DanteMode/queue.txt", "a") as image_queue:
                             if hook:
                                 image_queue.write("\n" + str(self.channel.id) + "|" + hook.url + "|" + str(image).replace("\n", "\\n"))
@@ -64,15 +67,15 @@ class SynapseTool(Tool):
     doc = "You are able to ask another assistant known as Synapse to search the web for the answer to a question. Simply enclose a prompt in $% and %$, like this: $%Who is Edna Mode?%$. Please only use this tool if you cannot answer the question or the user has requested it."
     def __init__(self, requests):
         super().__init__()
-        self.regex = re.compile(r'<-[\S\s]+>')
+        self.regex = re.compile(r'<-[\S\s]+?>')
         self.requests = requests
     def filter(self, answer):
-        return re.sub(r'<-[\S\s]+>', "", answer)
+        return re.sub(r'<-[\S\s]+?>', "", answer)
     def will_run(self, answer):
         if self.regex.search(answer):
             return True
         else:
             return False
-    def run(self, answer):
+    async def run(self, answer):
         for image in re.findall(self.regex, answer):
             pass
