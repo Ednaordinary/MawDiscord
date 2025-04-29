8e446cd51952e75c3b0a835cc3b30136dab78145
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-04-28 22:13:45 -0600

    make counter more resilient (and work?)

--- modeling/counter.py
+++ modeling/counter.py
@@ -8,12 +8,18 @@ class RequestsCounter:
         while self.block == True:
             time.sleep(0.01)
         self.block = True
-        with open(self.path, 'rb') as file:
-            counter = int.from_bytes(file.read(), byteorder='big')
+        try:
+            with open(self.path, 'rb') as file:
+                counter = int.from_bytes(file.read(), byteorder='big')
+        except:
+            counter = 0
         counter += 1
         with open(self.path, 'wb') as file:
-            file.write((i).to_bytes((max(i.bit_length() + 7, 1) // 8), byteorder='big', signed=False))
+            file.write((counter).to_bytes((max(counter.bit_length() + 7, 1) // 8), byteorder='big', signed=False))
     def get(self):
-        with open(self.path, 'rb') as file:
-            counter = int.from_bytes(file.read(), byteorder='big')
+        try:
+            with open(self.path, 'rb') as file:
+                counter = int.from_bytes(file.read(), byteorder='big')
+        except:
+            counter = 0
         return counter
