9ad9ba63c3e6a085bf166cb94fb0369737b5f929
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-07-12 00:23:17 -0600

    update exl3model to current real state

--- modeling/exl3model.py
+++ modeling/exl3model.py
@@ -1,4 +1,3 @@
-from exllamav3 import Model, Config, Cache, CacheLayer_quant, Tokenizer, AsyncGenerator, AsyncJob
 from multiprocessing import Queue
 import threading
 import traceback
@@ -10,63 +9,91 @@ import sys
 import gc
 import os
 
+from exllamav3 import Model, Config, Cache, CacheLayer_quant, Tokenizer, AsyncGenerator, AsyncJob, ComboSampler
+
 libc = ctypes.CDLL("libc.so.6")
 
-def run_loop(loop):
-    loop.run_forever()
+def run_loop(self):
+    try:
+        self.loop.run_forever()
+    except:
+        print(traceback.format_exc(), flush=True)
 
 class Exl3Loop:
     def __init__(self):
-        self.loop = asyncio.new_event_loop()
-        self.thread = threading.Thread(target=run_loop, args=[self.loop], daemon=True)
-        self.thread.start()
+        try:
+            self.loop = asyncio.new_event_loop()
+            #self.process = Process(target=run_loop, args=[self])
+            #self.process.start()
+            self.thread = threading.Thread(target=run_loop, args=[self], daemon=True)
+            self.thread.start()
+        except:
+            print(traceback.format_exc(), flush=True)
 
 class Exl3Engine:
     def __init__(self, model_id, cache_size, quant, loop, callback):
         self.config = Config.from_directory(model_id)
         self.model = Model.from_config(self.config)
-        self.cache = Cache(self.model, max_num_tokens=cache_size, layer_type=CacheLayer_quant, k_bits=quant[0], v_bits=quant[1])
+        if quant:
+            self.cache = Cache(self.model, max_num_tokens=cache_size, layer_type=CacheLayer_quant, k_bits=quant[0], v_bits=quant[1])
+        else:
+            self.cache = Cache(self.model, max_num_tokens=cache_size)
         self.model.load(progressbar=False, callback=callback)
         self.tokenizer = Tokenizer.from_config(self.config)
         self.engineloop = loop
         asyncio.run_coroutine_threadsafe(self._get_gen(), loop=self.engineloop.loop).result()
 
     async def _get_gen(self):
-        self.generator = AsyncGenerator(
-            model = self.model,
-            cache = self.cache,
-            tokenizer = self.tokenizer,
-        )
+        try:
+            self.generator = AsyncGenerator(
+                model = self.model,
+                cache = self.cache,
+                tokenizer = self.tokenizer,
+            )
+        except:
+            print(traceback.format_exc(), flush=True)
 
     async def _run_job(self, prompt, stop_token: str, add_bos: bool, max_tokens: int, sampler, queue: Queue):
         try:
             job = AsyncJob(
                 self.generator,
                 max_new_tokens = max_tokens,
-                token_healing=True,
+                token_healing=False,
                 gen_settings=sampler,
                 decode_special_tokens=True,
-                input_ids = self.tokenizer.encode(prompt, add_bos = False) if isinstance(prompt, str) else prompt,
+                input_ids = self.tokenizer.encode(prompt, add_bos = add_bos).to("cpu") if isinstance(prompt, str) else prompt.to("cpu"),
                 seed=random.randint(1, 10000000),
             )
+            think_switch = False
+            post_think = 0
             async for result in job:
                 text = result.get("text", "")
+                if think_switch == True:
+                    post_think += len(text)
+                if text == "</think>":
+                    think_switch = True
+                if post_think >= 2000: # Force stop after 2000 sent characters
+                    await job.cancel()
+                    queue.put(True)
+                    return
                 if stop_token in text:
                     await job.cancel()
-                    break
+                    queue.put(True)
+                    return
                 if text != None and text != "":
                     queue.put(text)
         except Exception as e:
             print("Exception in engine:")
             print(traceback.format_exc())
-        queue.put(None)
+        queue.put(False)
     
     def generate(self, prompt, stop_token, sampler, add_bos=True, max_tokens=256):
         queue = Queue()
         asyncio.run_coroutine_threadsafe(self._run_job(prompt, stop_token, add_bos, max_tokens, sampler, queue), self.engineloop.loop)
         while True:
             i = queue.get()
-            if i == None:
+            if isinstance(i, bool):
+                #yield i
                 break
             yield i
     
@@ -81,6 +108,7 @@ class Exl3Engine:
     
     def end(self):
         asyncio.run_coroutine_threadsafe(self._exit(), loop=self.engineloop.loop).result()
+        self.model.unload()
         del self.generator
         del self.model
         del self.cache
