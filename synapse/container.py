d21ac48498d338f4f152c1cd80fab0e1babfd409
BuildTools <imnotednamode@gmail.com>
2026-06-10 21:37:15 -0600

    ruff format

--- synapse/container.py
+++ synapse/container.py
@@ -9,11 +9,12 @@ client = podman.PodmanClient()
 with open(str(Path(__file__).parent) + "/config.json", "r") as config_file:
     client_config = ujson.decode(config_file.read())
 
+
 class Container:
     def __init__(self):
         self.container = client.containers.create(**client_config)
         self.users = 0
-        
+
     def run(self, cmd):
         self.users += 1
         if self.users == 1:
@@ -23,9 +24,9 @@ class Container:
             _, output_bytes = self.container.exec_run(cmd, demux=True)
             stdout_bytes, stderr_bytes = output_bytes
             stdout_bytes, stderr_bytes = stdout_bytes or b"", stderr_bytes or b""
-            stdout = ftfy.fix_text(stdout_bytes.decode("utf-8", errors='replace'))
+            stdout = ftfy.fix_text(stdout_bytes.decode("utf-8", errors="replace"))
             stdout = cleantext.clean(stdout, to_ascii=False, lower=False).strip()
-            stderr = ftfy.fix_text(stderr_bytes.decode("utf-8", errors='replace'))
+            stderr = ftfy.fix_text(stderr_bytes.decode("utf-8", errors="replace"))
             stderr = cleantext.clean(stderr, to_ascii=False, lower=False).strip()
             match stdout, stderr:
                 case "", "":
